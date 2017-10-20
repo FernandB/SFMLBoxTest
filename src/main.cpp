f8d6b9f1f564efb5a4714f06f14f470cbc876661
FernandB5 <battisti.fernand@gmail.com>
2017-10-20 16:17:18 +0200

    Mise en place du ground

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include <Box2D/Box2D.h>
+#define SCALE 30.0f
 
 int main()
 {
@@ -13,21 +14,45 @@ int main()
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	window.setFramerateLimit(60.f);
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color(2, 56, 37));
-	b2BodyDef myBodyDef;
-	myBodyDef.type = b2_dynamicBody; //this will be a dynamic body
-	myBodyDef.position.Set(0, 0); //set the starting position
-	b2Body* dynamicBody = myWorld->CreateBody(&myBodyDef);
-
+	
+	sf::RectangleShape square;
+	square.setFillColor(sf::Color::Blue);
+	square.setSize(sf::Vector2f(30.f,30.f));
+	b2PolygonShape b2square;
+	b2square.SetAsBox(30.0f,30.0f);
+	b2BodyDef squareBodyDef;
+	squareBodyDef.type = b2_dynamicBody; //this will be a dynamic body
+	squareBodyDef.position.Set(0.f, 50.f); //set the starting position
+	b2Body* squareBody = myWorld->CreateBody(&squareBodyDef);
+	b2FixtureDef squareFixtureDef;
+	squareFixtureDef.shape = &b2square;
+	squareFixtureDef.friction = 0;
+	squareFixtureDef.restitution = 0;
+	b2Fixture* squareFixture = squareBody->CreateFixture(&squareFixtureDef);
 	float32 timeStep = 1 / 60.0;      //the length of time passed to simulate (seconds)
 	int32 velocityIterations = 8;   //how strongly to correct velocity
 	int32 positionIterations = 3;   //how strongly to correct position
+	
+	b2PolygonShape b2ground;
+	b2ground.SetAsBox(800.0f, 0.f);
+	sf::RectangleShape ground;
+	ground.setFillColor(sf::Color::Green);
+	ground.setSize(sf::Vector2f(800.0f, 50.f));
+	b2BodyDef groundBodyDef;
+	groundBodyDef.type = b2_staticBody; //this will be a static body
+	groundBodyDef.position.Set(0.f, 550.f); //slightly lower position
+	b2Body* staticGroundBody = myWorld->CreateBody(&groundBodyDef); //add body to world
+	b2FixtureDef groundFixtureDef;
+	groundFixtureDef.shape = &b2ground;
+	groundFixtureDef.friction = 1;
+	groundFixtureDef.restitution = 0;
+	b2Fixture* groundFixture = staticGroundBody->CreateFixture(&groundFixtureDef);
+	ground.setPosition(staticGroundBody->GetPosition().x, staticGroundBody->GetPosition().y);
 
 	
-
+	
 	float speed = 5.0f;
-	Image image(std::string("data/image.png"));
+	
 	while (window.isOpen())
 	{
 		myWorld->Step(timeStep, velocityIterations, positionIterations);
@@ -45,14 +70,15 @@ int main()
 			}
 		}
 		
+	
+			square.setPosition(squareBody->GetPosition().x, squareBody->GetPosition().y);
 
-		shape.setPosition(dynamicBody->GetPosition().x, dynamicBody->GetPosition().y);
+		
 
 		window.clear();
 
-		image.draw(window);
-		window.draw(shape);
-		
+		window.draw(square);
+		window.draw(ground);
 		window.display();
 	}
 	delete myWorld;
